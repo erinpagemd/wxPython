1a881c14d776ec65751a5ab19f3200ebb5caa61c
erinpagemd <erinpagemd@gmail.com>
2014-04-25 19:56:07 -0500

    added buttons

--- FrameWithTextBox.py
+++ FrameWithTextBox.py
@@ -1,16 +1,28 @@
-#!/usr/bin/env python
 import wx
+
 class MyFrame(wx.Frame):
-    """ We simply derive a new class of Frame. """
-    def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(500,700))
-        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.CreateStatusBar()
 
+    def __init__(self):
+        wx.Frame.__init__(self, None, -1, "PayCheckPlanner", size=(300, 300))
+        panel = wx.Panel(self, -1)
 
+        #Enter Date
+        wx.StaticText(panel, -1, "Date", pos=(10, 10))
+        self.posCtrl = wx.TextCtrl(panel, -1, "", pos=(100, 10))
 
-        self.Show(True)
+        #Enter Title
+        wx.StaticText(panel, -1, "Title", pos=(10, 40))
+        self.posCtrl = wx.TextCtrl(panel, -1, "", pos=(100, 40))
 
-app = wx.App(False)
-frame = MyFrame(None, 'Paycheck Planner')
+        #Enter Amount
+        wx.StaticText(panel, -1, "Amount", pos=(10, 70))
+        self.posCtrl = wx.TextCtrl(panel, -1, "", pos=(100, 70))
+
+        #Expense/Income Button
+        button = wx.Button(panel, id=wx.ID_ANY, label="Income", pos=(60, 90))
+        button = wx.Button(panel, id=wx.ID_ANY, label="Expense", pos=(150, 90))
+
+app = wx.PySimpleApp()
+frame = MyFrame()
+frame.Show(True)
 app.MainLoop()
